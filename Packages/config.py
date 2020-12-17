9ceae71b5dbbf9cf2055737d2765c24856fc0542
Asiful Alam Fahim <47291401+Faheem2k19@users.noreply.github.com>
2020-12-17 19:28:48 +0600

    info added

--- Packages/config.py
+++ Packages/config.py
@@ -1,3 +1,5 @@
-CLIENT_ID = '8faa91cff7854d48a18e26a18c14b1bc'
-CLIENT_SECRET = 'da6278b52c2041e985f86af479d076a2'
-CLIENT_USERNAME = '22wyhb2s4re2vkp5xrpm7rfaa'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+CLIENT_USERNAME = ''
+
+#in this section provide your spotify information to get access of you library.
